4a0b7e807409e220cdd9f906322edb11a17bbf4d
pradeep-selva <pradeepselvaraju@gmail.com>
2021-12-27 19:14:05 +0530

    bg: interface fixes

--- src/contracts/interfaces/IVault.sol
+++ src/contracts/interfaces/IVault.sol
@@ -46,11 +46,6 @@ interface IVault is IERC20 {
 
     function strategiesCount() external view returns (uint256);
 
-    function withdrawalQueue(uint256 idx)
-        external
-        view
-        returns (address[] memory);
-
     function emergencyShutdown() external view returns (bool);
 
     function depositLimit() external view returns (uint256);
